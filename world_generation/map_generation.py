6a83dfadd3fafd1515d7441235c5539c5c7fef63
Daniella Tola <au513437@uni.au.dk>
2021-06-29 15:05:36 +0200

    cleaned dungeon generation code

--- world_generation/map_generation.py
+++ world_generation/map_generation.py
@@ -16,32 +16,27 @@ class MapGeneration2D:
         if self.occupancy_grid[cur_pos_x,cur_pos_y] != self.occupied:
             self.occupancy_grid[cur_pos_x,cur_pos_y] = self.occupied
             n_nodes -= 1
-        else:
-            n_nodes = n_nodes
         if n_nodes == 0: # base case
             return
         (x,y), direction = self._neighbour_direction(0,4) # we define four directions: N, E, S, W, corresponding to the numbers: 0,1,2,3
-        if cur_pos_x+x >= 0 and cur_pos_x+x < self.grid_size and cur_pos_y+y >= 0 and cur_pos_y+y < self.grid_size: # ensure we don't move out of the valid grid space
-            cur_pos_x += x
-            cur_pos_y += y
+        new_x, new_y = cur_pos_x+x, cur_pos_y+y
+        if new_x >= 0 and new_x < self.grid_size and new_y >= 0 and new_y < self.grid_size: # ensure we don't move out of the valid grid space
+            cur_pos_x = new_x
+            cur_pos_y = new_y
         logging.debug(f"{direction}")
-        return self.drunkard_walk(cur_pos_x, cur_pos_y, n_nodes)
+        self.drunkard_walk(cur_pos_x, cur_pos_y, n_nodes)
 
     def _neighbour_direction(self, lower_range, upper_range):
         """
-        maps from a direction number to x,y increment/decrements for a 2d grid
+        maps from a random number to x,y direction increment/decrements for a 2d grid
         """
         direction = random.randrange(lower_range,upper_range)
-        mapping = {0: 'N', 1: 'E', 2: 'S', 3: 'W', 4: 'NE', 5: 'SE', 6: 'SW', 7: 'NW'}
+        mapping = {0: 'N', 1: 'E', 2: 'S', 3: 'W'}
         mapping_direction = mapping[direction]
         direction_mapping = {'N': (-1,0),
                             'E': (0,1),
                             'S': (1,0),
-                            'W': (0,-1),
-                            'NE': (-1,1),
-                            'SE': (1,1),
-                            'SW': (1,-1),
-                            'NW': (-1,-1)}
+                            'W': (0,-1)}
         return direction_mapping[mapping_direction], mapping_direction
             
 
@@ -50,5 +45,5 @@ if __name__=="__main__":
     grid_size = 5
     mg = MapGeneration2D(grid_size, logging.DEBUG)
     start_pos = int(np.floor(grid_size/2))
-    mg.drunkard_walk(start_pos,start_pos,20)
+    mg.drunkard_walk(start_pos,start_pos,7)
     print(mg.occupancy_grid)
